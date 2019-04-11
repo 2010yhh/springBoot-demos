83897da2f1c359ea7c6aa149dacb8adccb88d7d2
yanhonghai <1160553161@qq.com>
2019-04-11 17:12:57 +0800

    修改sql名字

--- springboot-shiro/src/main/resources/test_shiro.sql
+++ springboot-shiro/src/main/resources/test_shiro.sql
@@ -10,12 +10,32 @@ Target Server Type    : MYSQL
 Target Server Version : 80000
 File Encoding         : 65001
 
-Date: 2018-11-19 18:01:09
+Date: 2018-12-18 18:36:51
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
-------------------
+-- ----------------------------
+-- Table structure for cas_user
+-- ----------------------------
+DROP TABLE IF EXISTS `cas_user`;
+CREATE TABLE `cas_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) DEFAULT NULL,
+  `pass_word` varchar(21000) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_name` (`user_name`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of cas_user
+-- ----------------------------
+INSERT INTO `cas_user` VALUES ('1', 'admin', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `cas_user` VALUES ('2', 'user1', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `cas_user` VALUES ('3', 'user2', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `cas_user` VALUES ('5', 'updateName', '123456');
+
+-- ----------------------------
 -- Table structure for sys_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_permission`;
@@ -102,6 +122,7 @@ CREATE TABLE `sys_user` (
 INSERT INTO `sys_user` VALUES ('238', 'admin', 'user104_RealName', '038bdaf98f2037b31f1e75b5b4c9b26e', '1111111', null, '111', '2018-10-09 18:50:03', '2018-09-28 19:28:06', null);
 INSERT INTO `sys_user` VALUES ('239', 'user1', 'user1_RealName', 'd6e75e048d00e4d0378d904bfdf81870', null, null, null, '2018-10-23 11:35:55', '2018-10-23 11:35:55', null);
 INSERT INTO `sys_user` VALUES ('240', 'user2', 'user2_RealName', '2fec513e98ba7033049be21d92f55fa3', null, null, null, '2018-10-23 14:29:51', '2018-10-23 14:29:51', null);
+INSERT INTO `sys_user` VALUES ('241', 'yhh', 'yhh', 'e22fdab996d9aca5be5ba6ae4f361255', null, null, null, '2018-11-13 22:37:08', '2018-11-13 22:37:08', null);
 
 -- ----------------------------
 -- Table structure for sys_user_role
@@ -117,3 +138,12 @@ CREATE TABLE `sys_user_role` (
   CONSTRAINT `sys_user_role_role_id` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`role_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `sys_user_role_user_id` FOREIGN KEY (`user_id`) REFERENCES `sys_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='用户-角色表';
+
+-- ----------------------------
+-- Records of sys_user_role
+-- ----------------------------
+INSERT INTO `sys_user_role` VALUES ('6', '238', '1');
+INSERT INTO `sys_user_role` VALUES ('7', '238', '2');
+INSERT INTO `sys_user_role` VALUES ('8', '238', '3');
+INSERT INTO `sys_user_role` VALUES ('9', '239', '2');
+INSERT INTO `sys_user_role` VALUES ('10', '240', '2');
