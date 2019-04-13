60136c1db24134aa984cc468dafc20963ddb1bd3
yanhonghai <1160553161@qq.com>
2019-04-14 01:02:45 +0800

    springboot-整合Vue，跨域，cookie跨域，session共享demo

--- springboot-cros/src/main/java/com/ctg/test/cros/springbootcros/SpringbootCrosApplication.java
+++ springboot-cros/src/main/java/com/ctg/test/cros/springbootcros/SpringbootCrosApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan
 public class SpringbootCrosApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringbootCrosApplication.class, args);
     }
